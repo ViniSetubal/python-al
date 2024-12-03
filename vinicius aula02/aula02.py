efaaf3424f0af9f648cd6c11e6af827f66e304e6
ViniSetubal <viniciusandradesetubal@gmail.com>
2024-12-03 09:59:28 -0300

    Add files via upload

---
+++ vinicius aula02/aula02.py
@@ -0,0 +1,9 @@
+print("bom dia a todos")
+nome_usuario="vinicius" #tipo -> str = stringS
+idade_usuario=19 #tipo -> int = integer
+altura_usuario=1.70 #tipo -> float = ponto flutuante
+maior_idade=True #tipo -> bool = boolean
+print("seja bem-vindo", nome_usuario)
+print("sua idade é", idade_usuario, "anos")
+print("sua altura é, altura-usuario,"metros.")
+print("você é maior de idade?", maior_idade)    
